1e8576fbcbc5c5a7a311217611cd4a3d41152a40
musicismrita <musicismrita@gmail.com>
2026-08-08 11:01:38 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='a4tool-v16';const A=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','html2canvas.min.js','jspdf.umd.min.js'];
+const C='a4tool-v18';const A=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','html2canvas.min.js','jspdf.umd.min.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
